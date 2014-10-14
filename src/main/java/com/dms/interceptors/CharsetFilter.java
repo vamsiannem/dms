b3281a053178327c69b593a62d17b7c09c6f968d
Vamsi Krishna Annem <vamsikrishna.annem@listertechnologies.com>
2014-10-14 18:58:40 +0530

    first commit

---
+++ src/main/java/com/dms/interceptors/CharsetFilter.java
@@ -0,0 +1,27 @@
+package com.dms.interceptors;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CharsetFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		servletRequest.setCharacterEncoding("UTF-8");
+		servletResponse.setCharacterEncoding("UTF-8");
+		servletResponse.setContentType("text/html; charset=UTF-8");
+
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		response.setHeader("cache-control", "no-cache, no-store");
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
