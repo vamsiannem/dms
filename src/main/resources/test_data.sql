dea37b98e9c2af1e03d2695f29b10f9125f53653
VamsiKrishna Annem <vamsikrishna.annem@listertechnologies.com>
2015-04-16 07:09:50 +0530

    Added test data

--- src/main/resources/test_data.sql
+++ src/main/resources/test_data.sql
@@ -2,6 +2,8 @@
 # Sample Users 
 INSERT INTO `client_info` (`id`, `address`, `contact_no`, `contact_person`, `name`) VALUES (1,'Mumbai',813948773,'admin','Hiranandani');
 
+insert into product_info(part_no, unit_serial_no, description) values ('2342', '1234', 'VLIFE MK1');
+
 INSERT INTO `permission` (`id`, `description`, `name`) VALUES (7,'Permission to upload a CSV file.','UPLOAD_CSV'),(8,'Permission to view CSV data in the form of graphs.','VIEW_CSV_CHARTS'),(9,'Permission to manage users.','MANAGE_USER'),(10,'Permission to view CSV data in the form of graphs.','VIEW_CSV_CHARTS'),(11,'Permission to manage users.','MANAGE_USER'),(12,'Permission to upload a CSV file.','UPLOAD_CSV');
 
 INSERT INTO `role` (`id`, `name`) VALUES (3,'ADMIN'),(4,'ADMIN');
